ca40b66a71126e0492bbbfde30b56efe2537d36a
Ervin Ramic <ervin@flexpos.com>
2017-12-18 15:31:17 +0100

    added new photos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  API_ENDPOINT: 'https://theglobalx.firebaseapp.com/',
+  API_ENDPOINT: 'https://www.theglobalx.com/',
+  WEB_ENDPOINT: 'http://localhost:65252/',
   AppName: 'AngularSpree'
 };
