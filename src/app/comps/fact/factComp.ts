6ac652ee7562f4b3a47057ef2ab300740d9c3a0e
unknown <zinr0ck69@gmail.com>
2018-06-03 11:46:58 +0200

    go

--- src/app/comps/fact/factComp.ts
+++ src/app/comps/fact/factComp.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
   selector: 'factComp',
@@ -11,5 +13,28 @@ export class FactComp implements OnInit {
   constructor() { }
   
   ngOnInit() {
+
+    $(".read-more").on("click", function(){
+      
+      var text_more;
+      var content_collapse = $(this).data("target");
+      var text_close = $("[data-collapse="+content_collapse+"]").data("text-close");
+
+      if(!$(this).hasClass('collapse-open')){
+        $(this).data("text-more", $(this).html());
+          text_more = $(this).data("text-more");
+          $(this).addClass('collapse-open');	
+          $(this).html(text_close);
+          $("[data-collapse="+content_collapse+"]").slideDown(300);
+          
+                    
+        } else {
+          text_more = $(this).data("text-more");
+          $(this).html(text_more);	
+          $(this).removeClass('collapse-open');
+          $("[data-collapse="+content_collapse+"]").slideUp(300);
+        }
+      });
+
   }
 }
